c0556a3d14017adeadd4ead6d24bb25b758f8581
oğuzhan kömcü <komcuoguzz@gmail.com>
2022-02-23 22:35:21 +0300

    I created a sample database with N-Tier Architecture.

--- CSharp_N-TİER_ARCHİTECTURE/Entities/Concrete/Loan.cs
+++ CSharp_N-TİER_ARCHİTECTURE/Entities/Concrete/Loan.cs
@@ -7,7 +7,18 @@ using System.Threading.Tasks;
 
 namespace CSharp_N_TİER_ARCHİTECTURE.Entities.Concrete
 {
-    public class Loan :BaseEntity
+    public class Loan : BaseEntity
     {
+        public int Customer_Id { get; set; }
+        public virtual Customer Customer { get; set; }
+
+        public int Branch_Id { get; set; }
+        public virtual Branch Branch { get; set; }
+
+        public string Loan_Amount { get; set; }
+        public DateTime? Date_Issued { get; set; }
+
+
+
     }
 }
--- CSharp_N-TİER_ARCHİTECTURE/Entities/Concrete/Transaction.cs
+++ CSharp_N-TİER_ARCHİTECTURE/Entities/Concrete/Transaction.cs
@@ -8,5 +8,20 @@ namespace CSharp_N_TİER_ARCHİTECTURE.Entities.Concrete
 {
     public class Transaction
     {
+        public string Transaction_Type { get; set; }
+        public int  From_Account_Id { get; set; }
+        public virtual Account Account { get; set; }
+        public int To_Account_Id { get; set; }
+        public DateTime? Date_İssued { get; set; }
+        public string  Amount { get; set; }
+
+
+        public string Transaction_Medium { get; set; }
+
+
+
+
+
+
     }
 }
