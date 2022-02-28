f3c1521fe9f8abc5d15d9156151e0692d57a4ace
oğuzhan kömcü <komcuoguzz@gmail.com>
2022-03-01 00:05:25 +0300

    I made an example of the code first approach.

--- Code_First/Form1.cs
+++ Code_First/Form1.cs
@@ -16,5 +16,10 @@ namespace Code_First
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
